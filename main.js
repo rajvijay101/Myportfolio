eae4a96a55c5397c26149b1eed6b92b8f1c4a278
Rajvijay yadav <132844861+rajvijay101@users.noreply.github.com>
2024-08-27 16:21:43 +0530

    Add files via upload
    My portfolio

---
+++ main.js
@@ -0,0 +1,11 @@
+// var typed= new Typed(".text", {
+//     Strings:["Frontend Developer",  " webdeveloper", "webdesiging"],
+//     typedSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+var typed = new Typed('.text', {
+    strings: ['Web Developer.','Frontend Developer','Backend Developer'],
+    typeSpeed: 50,
+  });
